3e5f05bb1ba0e87521fedb39cfaf29b84f9ccbf8
Adarsh Bachewale <abachewale@dataeconomy.ai>
2026-01-27 15:18:10 +0530

    made changes to include the airbnb data processing

--- scripts/Pyspark_basic_test.py
+++ scripts/Pyspark_basic_test.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
-spark = SparkSession.builder.appName("HelloSpark").getOrCreate()
+spark = SparkSession.builder.appName("AirBnb Processing").getOrCreate()
 
 # Read sample data from S3 (replace with your bucket later)
 df_listings = spark.read.format('csv')\
@@ -9,5 +9,14 @@ df_listings = spark.read.format('csv')\
           .option('inferSchema', True)\
           .load("s3://project-data-adarshpractice/AirBnb_Data/Listings.csv")
 
-# Show first few rows
-df_listings.show()
+df_superhost = df_listings.filter(col("host_is_superhost") == 't')\
+                          .groupBy(col("city"))\
+                          .agg(count(col("listing_id")).alias("host_count"))
+
+df_result = df_listings.join(broadcast(df_superhost),"city", 'left')\
+                       .groupBy(col("city"))\
+                       .agg(count("listing_id").alias("total_count"),max("host_count").alias("superhost_count"))\
+                       .fillna("No Super Host", subset = ['city'])\
+                       .select(col("city"),(col("superhost_count") / col("total_count")) * 100)\
+
+df_result.write.format('parquet').path("s3://project-data-adarshpractice/AirBnb_Data/processed_data")
